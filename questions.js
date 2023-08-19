6bafca3898ef24ec4e0d04ace82fcd4310f6ae80
RUTUJABHALEKAR <99064660+RUTUJABHALEKAR@users.noreply.github.com>
2023-08-19 23:38:31 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What is the next prime number after 7 ?",
+  answer: "11",
+  options: [
+    "13",
+    "12",
+    "11",
+    "14"
+  ]
+},
+  {
+  numb: 2,
+  question: "121 divided by 11 is",
+  answer: "11",
+  options: [
+    "18",
+    "10",
+    "19",
+    "11"
+  ]
+},
+  {
+  numb: 3,
+  question: "What is the product of 131*0*300*4",
+  answer: "0",
+  options: [
+    "11",
+    "0",
+    "46",
+    "45"
+  ]
+},
+  {
+  numb: 4,
+  question: "Solve 23 + 3 / 3",
+  answer: "24",
+  options: [
+    "24",
+    "25",
+    "26",
+    "27"
+  ]
+},
+  {
+  numb: 5,
+  question: "What is 6% equals to",
+  answer: "0.06",
+  options: [
+    "0.6",
+    "0.06",
+    "0.006",
+    "0.0006"
+  ]
+},
+];
